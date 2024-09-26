Bugfix: Wrong number of batches for computation of P

compare_laplace_approximations.py
@@ -13,6 +13,7 @@ from torch.utils.data import DataLoader
 import numpy as np
 import laplace
 from laplace import FullLaplace, KronLaplace
+from tqdm import tqdm
 
 from projector.projector1d import create_jacobian_data_iterator
 from projector.fisher import get_V_iterator
@@ -153,14 +154,14 @@ def run_main(cfg: DictConfig) -> None:
     create_train_proj_jac_it = lambda: create_jacobian_data_iterator(dataset=train_data,
                                                     model=model,
                                                     batch_size=projector_batch_size,
-                                                    number_of_batches=projector_batch_size,
+                                                    number_of_batches=projector_number_of_batches,
                                                     device=device,
                                                     dtype=dtype,
                                                     chunk_size=chunk_size)
     create_test_proj_jac_it = lambda: create_jacobian_data_iterator(dataset=test_data,
                                                     model=model,
                                                     batch_size=projector_batch_size,
-                                                    number_of_batches=projector_batch_size,
+                                                    number_of_batches=projector_number_of_batches,
                                                     device=device,
                                                     dtype=dtype,
                                                     chunk_size=chunk_size)
@@ -322,7 +323,8 @@ def run_main(cfg: DictConfig) -> None:
                                             J_X=create_test_proj_jac_it,
                                             s_iterable=s_list)
             assert callable(create_Sigma_P_s_it) 
-            for s, Sigma_P_s in zip(s_list, create_Sigma_P_s_it()):
+            for s, Sigma_P_s in tqdm(zip(s_list, create_Sigma_P_s_it()),
+                                     desc='Running through s'):
                 update_performance_metrics(
                     metrics_dict=results[seed]['low_rank'][method]['metrics'],
                     Sigma_approx=Sigma_P_s, Sigma=Sigma_ref)

utils.py
@@ -217,7 +217,8 @@ def iterator_wise_quadratic_form(quadratic_form: Callable[[torch.Tensor, torch.T
         A torch.tensor matrix
     """
     rows = []
-    for i, X_i in tqdm(enumerate(create_iterator())):
+    for i, X_i in tqdm(enumerate(create_iterator()),
+                           desc='iterator_wise_quadratic_form'):
         columns = []
         if number_of_batches is not None:
             if i >= number_of_batches:
@@ -258,7 +259,8 @@ def iterator_wise_matmul(create_a_iterator: Callable[[], Iterable],
     assert b.ndim in [1,2]
     if iteration_dim==0:
         a_times_b_collection = []
-        for i, a_i in enumerate(create_a_iterator()):
+        for i, a_i in tqdm(enumerate(create_a_iterator()),
+                           desc='iterator_wise_matmul'):
             if number_of_batches is not None:
                 if i >= number_of_batches:
                     break
@@ -271,7 +273,8 @@ def iterator_wise_matmul(create_a_iterator: Callable[[], Iterable],
     elif iteration_dim==1:
         a_times_b = 0.0
         batch_start_index = 0
-        for i, a_i in enumerate(create_a_iterator()):
+        for i, a_i in tqdm(enumerate(create_a_iterator()),
+                           desc='iterator_wise_matmul'):
             if number_of_batches is not None:
                 if i >= number_of_batches:
                     break
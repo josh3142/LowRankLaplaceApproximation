e87e2801c3d0164ede35206eff9a676f6ee083dd
homunkulus <josua.faller@ptb.de>
2024-12-06 13:52:32 +0100

    Add second colour scheme

--- color_map.py
+++ color_map.py
@@ -11,14 +11,29 @@ Color = Union[
 cm = plt.cm.Dark2
 
 
-colors = {
+colors1 = {
     'kron': cm(0),
     'magnitude': cm(1),
     'swag': cm(2),
     'diagonal': cm(3),
-    'None': cm(4),
+    'None': 'black',
+}
+
+colors2 = {
+    'kron': cm(4),
+    'magnitude': cm(5),
+    'swag': cm(6),
+    'diagonal': plt.cm.Set1(8),
+    'None': 'grey',
 }
 
 
 def get_color(method: str) -> Color:
-    return colors.get(method, 'black')
+    if not method in colors1.keys():
+        raise NotImplementedError
+    return colors1.get(method, 'black')
+
+def get_color2(method: str) -> Color:
+    if not method in colors2.keys():
+        raise NotImplementedError
+    return colors2.get(method, 'black')
